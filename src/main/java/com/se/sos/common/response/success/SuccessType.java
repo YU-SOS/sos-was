eaf9ddf01ade7dac1864d65cdcc7c3383b65fad4
hky035 <rldud035@naver.com>
2024-08-21 23:56:08 +0900

    feat: 성공 응답 설정

---
+++ src/main/java/com/se/sos/common/response/success/SuccessType.java
@@ -0,0 +1,25 @@
+package com.se.sos.common.response.success;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessType {
+
+    // 200
+    OK(HttpStatus.OK, "요청이 성공했습니다."),
+
+    // 201
+    CREATED(HttpStatus.CREATED, "등록을 성공하였습니다."),
+    USER_CREATED(HttpStatus.CREATED, "사용자 등록을 성공하였습니다."),
+    COMMENT_CREATED(HttpStatus.CREATED, "코멘트 등록을 성공하였습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    public int getStatusCode(){
+        return status.value();
+    }
+}
