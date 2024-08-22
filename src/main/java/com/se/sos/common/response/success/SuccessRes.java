251024db4fbe5f9eddd61e55714217d3fd723b89
hky035 <rldud035@naver.com>
2024-08-22 14:52:42 +0900

    refact: SuccessRes dto 클래스 record로 변경

--- src/main/java/com/se/sos/common/response/success/SuccessRes.java
+++ src/main/java/com/se/sos/common/response/success/SuccessRes.java
@@ -1,22 +1,16 @@
 package com.se.sos.common.response.success;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
-@Getter
-@AllArgsConstructor
-@RequiredArgsConstructor
-public class SuccessRes<T> {
-    private final int status;
-    private final String message;
-    private T data; // 필수 X
-
+public record SuccessRes<T> (
+    int status,
+    String message,
+    T data // 필수 X
+){
     public static ResponseEntity<SuccessRes<?>> from(SuccessType success) {
         return ResponseEntity
                 .status(success.getStatus())
-                .body(new SuccessRes<>(success.getStatusCode(), success.getMessage()));
+                .body(new SuccessRes<>(success.getStatusCode(), success.getMessage(), null));
     }
 
     public static <T> ResponseEntity<SuccessRes<T>> from(T data) {
