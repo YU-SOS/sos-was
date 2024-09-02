47627ee3f4c233c227bc5c5d9f258c1321a35fcf
Kiyoung Heo <128910345+hky035@users.noreply.github.com>
2024-09-02 16:22:43 +0900

    Feature: Jwt 토큰 발급 Util 클래스 생성 (#12)
    * feat: jwt 환경변수 추가
    
    * chore: jwt 라이브러리 추가
    
    * feat: JwtUtil 생성
    
    * test: 테스트코드 작성

---
+++ src/main/java/com/se/sos/global/util/jwt/JwtUtil.java
@@ -0,0 +1,81 @@
+package com.se.sos.global.util.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.time.Duration;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+    private final Duration accessTokenDuration;
+    private final Duration refreshTokenDuration;
+    private final String issuer;
+
+    public JwtUtil(
+        @Value("${jwt.secret-key}") String secretKey,
+        @Value("${jwt.access-expiration}") Duration accessTokenDuration,
+        @Value("${jwt.refresh-expiration}") Duration refreshTokenDuration,
+        @Value("${jwt.issuer}") String issuer
+    ){
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(), Jwts.SIG.HS256.key().build().getAlgorithm());
+        this.accessTokenDuration = accessTokenDuration;
+        this.refreshTokenDuration = refreshTokenDuration;
+        this.issuer = issuer;
+    }
+
+    public String generateAccessToken(String userId, String role){
+        return Jwts.builder()
+                .subject(userId)
+                .issuedAt(new Date())
+                .issuer(issuer)
+                .expiration(createExpire(accessTokenDuration.toMillis()))
+                .claim("role", role)
+                .claim("tokenType", "access")
+                .compact();
+    }
+
+    public String generateRefreshToken(String userId, String role){
+        return Jwts.builder()
+                .subject(userId)
+                .issuedAt(new Date())
+                .issuer(issuer)
+                .expiration(createExpire(refreshTokenDuration.toMillis()))
+                .claim("role", role)
+                .claim("tokenType", "refresh")
+                .compact();
+    }
+
+    public Claims parseToken(String token){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getUsername(String token){
+        Claims claims = parseToken(token);
+        return claims.getSubject();
+    }
+
+    public String getRole (String token){
+        Claims claims = parseToken(token);
+        return claims.get("role", String.class);
+    }
+
+    public String getTokenType (String token){
+        Claims claims = parseToken(token);
+        return claims.get("tokenType", String.class);
+    }
+
+    private Date createExpire(Long expiration){
+        return new Date(new Date().getTime() + expiration);
+    }
+}
