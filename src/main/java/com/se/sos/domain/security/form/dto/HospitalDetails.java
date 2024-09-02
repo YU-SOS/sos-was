b5d67ae91d2a00994bbf912034aa6ef4522dc3ea
hky035 <rldud035@naver.com>
2024-09-03 02:13:14 +0900

    feat: HospitalDetails 생성

---
+++ src/main/java/com/se/sos/domain/security/form/dto/HospitalDetails.java
@@ -0,0 +1,39 @@
+package com.se.sos.domain.security.form.dto;
+
+import com.se.sos.domain.hospital.entity.Hospital;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class HospitalDetails implements CustomUserDetails {
+
+    private final Hospital hospital;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(hospital.getRole().toString()));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return hospital.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return hospital.getHospitalId();
+    }
+
+    @Override
+    public String getId() {
+        return hospital.getId();
+    }
+}
