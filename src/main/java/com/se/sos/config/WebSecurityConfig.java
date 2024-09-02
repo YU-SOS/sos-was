acdcb1f15f2e7f584fa29fad7bd1921e0e36d0ae
hky035 <rldud035@naver.com>
2024-09-02 17:47:25 +0900

    feat: cors 설정

--- src/main/java/com/se/sos/config/WebSecurityConfig.java
+++ src/main/java/com/se/sos/config/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.se.sos.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,12 +18,19 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity(debug=true)
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .formLogin(FormLoginConfigurer::disable);
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .formLogin(FormLoginConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+        ;
 
 
         return http.build();
