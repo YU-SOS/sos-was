e510268a460a68c84859a46a6a7e4af36675feb5
hky035 <rldud035@naver.com>
2024-09-03 02:00:43 +0900

    fix: 로그인을 위한 Ambulance 엔티티 변경

--- src/main/java/com/se/sos/domain/ambulance/entity/Ambulance.java
+++ src/main/java/com/se/sos/domain/ambulance/entity/Ambulance.java
@@ -1,7 +1,8 @@
 package com.se.sos.domain.ambulance.entity;
 
 import com.se.sos.domain.paramedic.entity.Paramedic;
-import com.se.sos.domain.reception.entity.Reception;
+//import com.se.sos.domain.reception.entity.Reception;
+import com.se.sos.global.common.role.Role;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -27,18 +28,32 @@ public class Ambulance {
     String address;
     String telephoneNumber;
 
+    String ambulanceId;
+    String password;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+
     // 위치 (경도 위도)
 
     @OneToMany(mappedBy = "ambulance")
     List<Paramedic> paramedics = new ArrayList<>();
 
-    @OneToMany(mappedBy = "ambulance")
-    List<Reception> receptions = new ArrayList<>();
+//    @OneToMany(mappedBy = "ambulance")
+//    List<Reception> receptions = new ArrayList<>();
+
+//    @Builder
+//    public Ambulance(String name, String address, String telephoneNumber, String ambulanceId, String password, String role) {
+//        this.name = name;
+//        this.address = address;
+//        this.telephoneNumber = telephoneNumber;
+//    }
 
     @Builder
-    public Ambulance(String name, String address, String telephoneNumber) {
-        this.name = name;
-        this.address = address;
-        this.telephoneNumber = telephoneNumber;
+    public Ambulance(String ambulanceId, String password, Role role) {
+        this.ambulanceId = ambulanceId;
+        this.password = password;
+        this.role = role;
     }
 }
