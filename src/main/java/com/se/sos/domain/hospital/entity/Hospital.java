578d8fb9207b62c784ad7037d50a8720fb648762
gojoongsu <gjs4565@naver.com>
2024-09-03 17:16:34 +0900

    진료과목과 receptions 추가

--- src/main/java/com/se/sos/domain/hospital/entity/Hospital.java
+++ src/main/java/com/se/sos/domain/hospital/entity/Hospital.java
@@ -1,14 +1,21 @@
 package com.se.sos.domain.hospital.entity;
 
+import com.se.sos.domain.reception.entity.Reception;
 import com.se.sos.global.common.role.Role;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PROTECTED;
+
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
 public class Hospital {
 
     @Id
@@ -20,6 +27,12 @@ public class Hospital {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Enumerated(EnumType.STRING)
+    Speciality speciality; // 현재는 하나의 진료과목만 담을 수 있어서 추후 처리 필요
+
+    @OneToMany(mappedBy = "hospital")
+    List<Reception> receptions = new ArrayList<>();
 
     @Builder
     public Hospital(String hospitalId, String password, Role role) {
