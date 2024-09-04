27fa1fe9148e1b70ba8c59b3770fb78b0fa91bf0
gojoongsu <gjs4565@naver.com>
2024-09-04 20:26:19 +0900

    feat: createAt 필드 자동 생성되게 작성

--- src/main/java/com/se/sos/domain/comment/entity/Comment.java
+++ src/main/java/com/se/sos/domain/comment/entity/Comment.java
@@ -5,30 +5,39 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
     @Id
     @GeneratedValue
     Long id;
 
     String description; // 내용
+
+    @CreatedDate
+    @Column(updatable = false)
     LocalDateTime createdAt; // 작성시간
+
+
 
     @ManyToOne
     @JoinColumn(name = "RECEPTION_ID")
     Reception reception;
 
     @Builder
-    public Comment(String description, LocalDateTime createdAt, Reception reception) {
+    public Comment(String description, Reception reception) {
         this.description = description;
-        this.createdAt = createdAt;
         this.reception = reception;
     }
 }
