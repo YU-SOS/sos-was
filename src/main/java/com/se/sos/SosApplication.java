8c6c5c059e3dce6c5769339387bad2df55db38e2
gojoongsu <gjs4565@naver.com>
2024-09-04 20:28:16 +0900

    feat: @EnableJpaAuditing test파일과 중복으로 인해 주석처리

--- src/main/java/com/se/sos/SosApplication.java
+++ src/main/java/com/se/sos/SosApplication.java
@@ -2,8 +2,10 @@ package com.se.sos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+//@EnableJpaAuditing  test파일에서 실행할 때는 주석 처리 필요
 public class SosApplication {
 
 	public static void main(String[] args) {
