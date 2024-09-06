74d01b9fa0cb2e62202c4ef37739d7b16ee3bc07
hky035 <rldud035@naver.com>
2024-09-06 19:54:21 +0900

    feat: cookie util 클래스 작성

---
+++ src/main/java/com/se/sos/global/util/cookie/CookieUtil.java
@@ -0,0 +1,22 @@
+package com.se.sos.global.util.cookie;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    @Value("${cookie.refresh-expiration}")
+    private int REFRESH_EXPIRATION;
+
+    public ResponseCookie addRefreshTokenCookie(String value){
+        return ResponseCookie.from("refreshToken", value)
+                .maxAge(REFRESH_EXPIRATION)
+                .secure(false)
+                .httpOnly(true)
+                .path("/")
+                .sameSite("None")
+                .build();
+    }
+}
