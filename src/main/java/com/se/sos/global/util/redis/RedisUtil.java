b60d959538a839538c2268726f8addb89660ec71
gojoongsu <gjs4565@naver.com>
2024-09-10 20:48:45 +0900

    feat: redis에서 refreshToken 삭제를 위한 코드 작성과 save시 만료기간 반영 추가

--- src/main/java/com/se/sos/global/util/redis/RedisUtil.java
+++ src/main/java/com/se/sos/global/util/redis/RedisUtil.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -12,19 +13,36 @@ public class RedisUtil {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public void save(String key, Object value) {
-        redisTemplate.opsForValue().set(key, value);
+    public void save(String key, Object value, long expiration) {
+        redisTemplate.opsForValue().set(key, value, expiration, TimeUnit.MILLISECONDS);
     }
 
-    public void saveExpire(String key, Long seconds){
+    public void saveExpire(String key, Long seconds) {
         redisTemplate.expire(key, Duration.ofSeconds(seconds));
     }
 
-    public Object get(String key){
+    public Object get(String key) {
         return redisTemplate.opsForValue().get(key);
     }
 
-    public void delete(String userId) {
-        redisTemplate.delete(userId);
+    public Boolean delete(String userId) {
+        if (containsKey(userId)) {
+            redisTemplate.delete(userId);
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    public void blacklistToken(String key, long remainingTime) {
+        redisTemplate.opsForValue().set(key, "blacklisted", remainingTime, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean containsKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+    public boolean isTokenBlacklisted(String key) {
+        return containsKey(key);
+    }
+
 }
