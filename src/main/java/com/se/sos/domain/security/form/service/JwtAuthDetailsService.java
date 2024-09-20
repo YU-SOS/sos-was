1732b5d8b49582cecffcf02548537464abd64544
hky035 <rldud035@naver.com>
2024-09-20 20:44:25 +0900

    feat: jwt 토큰 인증을 위한 커스텀 UserDetailsService 추가

--- src/main/java/com/se/sos/domain/security/form/service/JwtAuthDetailsService.java
+++ src/main/java/com/se/sos/domain/security/form/service/JwtAuthDetailsService.java
@@ -6,42 +6,52 @@ import com.se.sos.domain.hospital.entity.Hospital;
 import com.se.sos.domain.hospital.repository.HospitalRepository;
 import com.se.sos.domain.security.form.dto.AmbulanceDetails;
 import com.se.sos.domain.security.form.dto.HospitalDetails;
+import com.se.sos.domain.security.form.dto.SecurityUserDetails;
+import com.se.sos.domain.user.entity.User;
+import com.se.sos.domain.user.repository.UserRepository;
+import com.se.sos.global.common.role.Role;
 import com.se.sos.global.exception.CustomException;
 import com.se.sos.global.response.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
-@Primary
-@Qualifier("loginDetailsService")
-@RequiredArgsConstructor
-public class AmbulanceAndHospitalDetailsService implements UserDetailsService {
+import java.util.UUID;
 
+@Service
+@Qualifier("jwtDetailsService")
+@RequiredArgsConstructor
+public class JwtAuthDetailsService implements UserDetailsService {
     private final HospitalRepository hospitalRepository;
     private final AmbulanceRepository ambulanceRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // role + subject(uuid)
         String role = username.split(" ")[0];
-        String id = username.split(" ")[1];
+        UUID id =  UUID.fromString(username.split(" ")[1]);
 
-        if(role.equals("AMB")){
+        if(role.equals(Role.AMB.getRole()) || role.equals(Role.AMB_GUEST.getRole())){
 
-            Ambulance ambulance = ambulanceRepository.findByAmbulanceId(id)
+            Ambulance ambulance = ambulanceRepository.findById(id)
                     .orElseThrow(() -> new CustomException(ErrorType.AMBULANCE_NOT_FOUND));
-            
+
             return new AmbulanceDetails(ambulance);
 
-        } else if( role.equals("HOS")){
-            Hospital hospital = hospitalRepository.findByHospitalId(id)
+        } else if( role.equals(Role.HOS.getRole()) || role.equals(Role.HOS_GUEST.getRole())){
+            Hospital hospital = hospitalRepository.findById(id)
                     .orElseThrow(() -> new CustomException(ErrorType.HOSPITAL_NOT_FOUND));
 
             return new HospitalDetails(hospital);
+        } else if ( role.equals(Role.USER.getRole())){
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new CustomException(ErrorType.USER_NOT_FOUND));
+
+            return new SecurityUserDetails(user);
         }
 
         return null;
