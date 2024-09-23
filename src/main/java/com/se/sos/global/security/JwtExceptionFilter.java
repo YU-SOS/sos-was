d42e195f31d9002a73f1febc3918b4f1ccf46a69
hky035 <rldud035@naver.com>
2024-09-23 16:25:11 +0900

    refactor: objectMapper 싱글톤 원칙 적용

--- src/main/java/com/se/sos/global/security/JwtExceptionFilter.java
+++ src/main/java/com/se/sos/global/security/JwtExceptionFilter.java
@@ -1,4 +1,4 @@
-package com.se.sos.domain.security.filter;
+package com.se.sos.global.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.se.sos.global.response.error.ErrorRes;
@@ -9,33 +9,36 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
+@RequiredArgsConstructor
 public class JwtExceptionFilter extends OncePerRequestFilter {
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-    ObjectMapper objectMapper =  new ObjectMapper();
-
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         ErrorType error = null;
 
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
         try{
             filterChain.doFilter(request, response);
         } catch (ExpiredJwtException e){
-            logger.error("token expired");
+            log.error("token expired");
             error = ErrorType.TOKEN_EXPIRED;
-            // response setStatus, setContentType, setChracterEncoding ?
             response.getWriter().write(objectMapper.writeValueAsString(ErrorRes.of(error.getStatusCode(), e.getMessage())));
         } catch (JwtException e){
-            logger.error("token authentication failed : " + e.getMessage()); // e출력?
+            log.error("token authentication failed : " + e.getMessage()); // e출력?
             error = ErrorType.UN_AUTHENTICATION;
             response.getWriter().write(objectMapper.writeValueAsString(ErrorRes.of(error.getStatusCode(), e.getMessage())));
-        } // finally는 catch로 에러가 안 잡히더라도 실행이 되는건가?
+        }
     }
 }
