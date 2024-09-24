cfaefba5a706c36cd0efa19ec25a5569e42c3952
gojoongsu <gjs4565@naver.com>
2024-09-25 01:33:25 +0900

    fix: 깃 충돌로 인한 수정

--- src/main/java/com/se/sos/domain/hospital/entity/Hospital.java
+++ src/main/java/com/se/sos/domain/hospital/entity/Hospital.java
@@ -7,9 +7,12 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import com.se.sos.domain.category.entity.Category;
+import com.se.sos.domain.category.entity.CategoryHospital;
+import com.se.sos.domain.category.repository.CategoryHospitalRepository;
+import com.se.sos.domain.comment.entity.Comment;
 import java.util.ArrayList;
-import java.util.List;
+import java.utigl.List;
 
 import static lombok.AccessLevel.PROTECTED;
 
