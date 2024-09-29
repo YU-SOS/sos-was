937d6f39e421b8608d7de59b9aa872ad51d6e14c
hky035 <rldud035@naver.com>
2024-09-29 17:47:30 +0900

    fix: 회원가입 목록 조회, 상세조회 GET 요청으로 통일

--- src/main/java/com/se/sos/domain/admin/controller/AdminController.java
+++ src/main/java/com/se/sos/domain/admin/controller/AdminController.java
@@ -2,8 +2,11 @@ package com.se.sos.domain.admin.controller;
 
 import com.se.sos.domain.admin.service.AdminService;
 import com.se.sos.domain.user.entity.Role;
+import com.se.sos.global.response.error.ErrorRes;
+import com.se.sos.global.response.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -16,12 +19,17 @@ public class AdminController {
     private final AdminService adminService;
 
     @GetMapping("/registration")
-    public ResponseEntity<?> getAllRegistration(){
-        return adminService.getAllRegistration();
+    public ResponseEntity<?> getRegistration(
+            @RequestParam(name = "role", required = false) Role role,
+            @RequestParam(name = "id", required = false) UUID id
+    ){
+        if (role == null && id == null)
+            return adminService.getAllRegistration();
+        else if(role != null && id != null)
+            return adminService.getRegistration(role, id);
+        else
+            return ResponseEntity.status(ErrorType.BAD_REQUEST.getStatus())
+                    .body(ErrorRes.from(ErrorType.BAD_REQUEST));
     }
 
-    @PostMapping("/registration")
-    public ResponseEntity<?> addRegistration(@RequestParam(name = "role") Role role, @RequestParam(name = "id") UUID id){
-        return adminService.getRegistration(role, id);
-    }
 }
