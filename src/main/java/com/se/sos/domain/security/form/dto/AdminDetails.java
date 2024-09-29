1a6c6b7c8e24fa13bc4d86e5bedba6774c1a46ad
hky035 <rldud035@naver.com>
2024-09-29 17:28:16 +0900

    feat: Admin UserDetails 생성

---
+++ src/main/java/com/se/sos/domain/security/form/dto/AdminDetails.java
@@ -0,0 +1,39 @@
+package com.se.sos.domain.security.form.dto;
+
+import com.se.sos.domain.admin.entity.Admin;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class AdminDetails implements CustomUserDetails{
+
+    private final Admin admin;
+
+    @Override
+    public String getId() {
+        return admin.getId().toString();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(admin.getRole().getRole()));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return admin.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return admin.getAdminId();
+    }
+}
