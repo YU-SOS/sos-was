28084dcdb7cdd909bd7872ba4ba6421051e66faf
hky035 <rldud035@naver.com>
2024-09-29 17:07:18 +0900

    feat: 회원가입 요청 상세 조회 api

--- src/main/java/com/se/sos/domain/admin/controller/AdminController.java
+++ src/main/java/com/se/sos/domain/admin/controller/AdminController.java
@@ -1,11 +1,12 @@
 package com.se.sos.domain.admin.controller;
 
 import com.se.sos.domain.admin.service.AdminService;
+import com.se.sos.domain.user.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/admin")
@@ -17,5 +18,10 @@ public class AdminController {
     @GetMapping("/registration")
     public ResponseEntity<?> getAllRegistration(){
         return adminService.getAllRegistration();
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> addRegistration(@RequestParam(name = "role") Role role, @RequestParam(name = "id") UUID id){
+        return adminService.getRegistration(role, id);
     }
 }
--- src/main/java/com/se/sos/domain/admin/service/AdminService.java
+++ src/main/java/com/se/sos/domain/admin/service/AdminService.java
@@ -1,9 +1,13 @@
 package com.se.sos.domain.admin.service;
 
 import com.se.sos.domain.admin.dto.RegSummaryRes;
+import com.se.sos.domain.ambulance.entity.Ambulance;
 import com.se.sos.domain.ambulance.repository.AmbulanceRepository;
+import com.se.sos.domain.hospital.entity.Hospital;
 import com.se.sos.domain.hospital.repository.HospitalRepository;
 import com.se.sos.domain.user.entity.Role;
+import com.se.sos.global.exception.CustomException;
+import com.se.sos.global.response.error.ErrorType;
 import com.se.sos.global.response.success.SuccessRes;
 import com.se.sos.global.response.success.SuccessType;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -40,5 +45,25 @@ public class AdminService {
 
         return ResponseEntity.status(SuccessType.OK.getStatus())
                 .body(SuccessRes.from(map));
+    }
+
+    public ResponseEntity<?> getRegistration(Role role, UUID id){
+
+        if(role.equals(Role.AMB_GUEST)){
+            Ambulance ambulance = ambulanceRepository.findById(id)
+                    .orElseThrow(() -> new CustomException(ErrorType.AMBULANCE_NOT_FOUND));
+
+            return ResponseEntity.status(SuccessType.OK.getStatus())
+                    .body(SuccessRes.from(ambulance));
+
+        } else if(role.equals(Role.HOS_GUEST)){
+            Hospital hospital = hospitalRepository.findById(id)
+                    .orElseThrow(() -> new CustomException(ErrorType.HOSPITAL_NOT_FOUND));
+
+            return ResponseEntity.status(SuccessType.OK.getStatus())
+                    .body(SuccessRes.from(hospital));
+        } else {
+            throw new CustomException(ErrorType.BAD_REQUEST);
+        }
     }
 }
