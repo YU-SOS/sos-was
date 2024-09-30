9c0ad8602ab999acbbc7ef933ee3a358aef29f15
gojoongsu <gjs4565@naver.com>
2024-09-30 20:44:15 +0900

    feat: CategoryHospitalRepository Pageable 추가 작성

--- src/main/java/com/se/sos/domain/category/repository/CategoryHospitalRepository.java
+++ src/main/java/com/se/sos/domain/category/repository/CategoryHospitalRepository.java
@@ -4,6 +4,8 @@ import com.se.sos.domain.category.entity.Category;
 import com.se.sos.domain.category.entity.CategoryHospital;
 import com.se.sos.domain.hospital.entity.Hospital;
 import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,7 +17,8 @@ public interface CategoryHospitalRepository extends JpaRepository<CategoryHospit
             "WHERE ch.category IN :categories " +
             "GROUP BY ch.hospital " +
             "HAVING COUNT(DISTINCT ch.category) = :categorySize")
-    List<Hospital> findHospitalsByCategories(@Param("categories") List<Category> categories,
-                                                @Param("categorySize") int categorySize);
+    Page<Hospital> findHospitalsByCategories(@Param("categories") List<Category> categories,
+                                             @Param("categorySize") int categorySize,
+                                             Pageable pageable);
 
 }
