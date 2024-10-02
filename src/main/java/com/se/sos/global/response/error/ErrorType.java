5a286b713b4079ef4a957231b4da2be78b81f4c6
hky035 <rldud035@naver.com>
2024-10-02 20:50:03 +0900

    feat: PARAMEDIC_NOT_FOUND 추가

--- src/main/java/com/se/sos/global/response/error/ErrorType.java
+++ src/main/java/com/se/sos/global/response/error/ErrorType.java
@@ -33,6 +33,7 @@ public enum ErrorType {
     AMBULANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 구급대입니다."),
     HOSPITAL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 병원입니다."),
     RECEPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 접수 입니다."),
+    PARAMEDIC_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 구급대원 입니다."),
 
     // 데이터 충돌
     ALREADY_USED_ID(HttpStatus.CONFLICT, "이미 사용 중인 아이디입니다."),
