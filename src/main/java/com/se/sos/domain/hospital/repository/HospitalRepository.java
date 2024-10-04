58e109c1f79dba7364ed189e2b9970d9efcd36c0
gojungsu <gjs4565@naver.com>
2024-10-05 00:03:04 +0900

    feat: HospitalRepository 필요한 로직 추가

--- src/main/java/com/se/sos/domain/hospital/repository/HospitalRepository.java
+++ src/main/java/com/se/sos/domain/hospital/repository/HospitalRepository.java
@@ -1,6 +1,5 @@
 package com.se.sos.domain.hospital.repository;
 
-import com.se.sos.domain.ambulance.entity.Ambulance;
 import com.se.sos.domain.hospital.entity.Hospital;
 import com.se.sos.domain.user.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +11,7 @@ import java.util.UUID;
 public interface HospitalRepository extends JpaRepository<Hospital, UUID> {
     Optional<Hospital> findById(UUID id);
     Optional<Hospital> findByHospitalId(String hospitalId);
-
+    Optional<Hospital> findByName(String name);
     List<Hospital> findByRole(Role role);
 
     boolean existsByHospitalId(String hospitalId);
