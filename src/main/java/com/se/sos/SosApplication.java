fee3438373578a2e06ba7a942d4b1fdcda1574b0
joongsu <112760801+joongsu@users.noreply.github.com>
2024-10-05 23:37:47 +0900

    Feature: reception 생성 (#56)
    * feat: receptionController 작성
    
    * feat: reception 생성 dto 작성
    
    * feat: ReceptionService 작성
    
    * feat: reception에 대한 허용 코드 추가
    
    * feat: 토큰으로부터 ambulanceid 얻는 로직 추가
    
    * feat: HospitalRepository 필요한 로직 추가
    
    * feat: 페이지 인터페이스의 직렬화 문제로 인한 코드

--- src/main/java/com/se/sos/SosApplication.java
+++ src/main/java/com/se/sos/SosApplication.java
@@ -3,9 +3,13 @@ package com.se.sos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.*;
 
 @SpringBootApplication
 //@EnableJpaAuditing  test파일에서 실행할 때는 주석 처리 필요
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class SosApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/se/sos/domain/reception/service/ReceptionService.java
@@ -0,0 +1,37 @@
+package com.se.sos.domain.reception.service;
+
+import com.se.sos.domain.ambulance.entity.Ambulance;
+import com.se.sos.domain.ambulance.repository.AmbulanceRepository;
+import com.se.sos.domain.hospital.entity.Hospital;
+import com.se.sos.domain.hospital.repository.HospitalRepository;
+import com.se.sos.domain.reception.dto.ReceptionCreateReq;
+import com.se.sos.domain.reception.entity.Reception;
+import com.se.sos.domain.reception.repository.PatientRepository;
+import com.se.sos.domain.reception.repository.ReceptionRepository;
+import com.se.sos.global.exception.CustomException;
+import com.se.sos.global.response.error.ErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ReceptionService {
+    private final ReceptionRepository receptionRepository;
+    private final AmbulanceRepository ambulanceRepository;
+    private final HospitalRepository hospitalRepository;
+    private final PatientRepository patientRepository;
+
+    public void createReception(ReceptionCreateReq receptionCreateReq, UUID ambulanceId) {
+        Ambulance ambulance = ambulanceRepository.findById(ambulanceId)
+                .orElseThrow(() -> new CustomException(ErrorType.AMBULANCE_NOT_FOUND));
+        Hospital hospital = hospitalRepository.findByName(receptionCreateReq.getHospitalName())
+                .orElseThrow(() -> new CustomException(ErrorType.HOSPITAL_NOT_FOUND));
+
+        Reception reception = ReceptionCreateReq.toEntity(receptionCreateReq, ambulance, hospital);
+        patientRepository.save(reception.getPatient());
+        receptionRepository.save(reception);
+    }
+
+}
