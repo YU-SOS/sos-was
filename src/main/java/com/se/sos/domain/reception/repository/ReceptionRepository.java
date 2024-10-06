d9f400e02de9ad773a226a772622d95bb794f83c
gojungsu <gjs4565@naver.com>
2024-10-06 23:20:42 +0900

    feat: id로 reception 찾는 메소드 추가

--- src/main/java/com/se/sos/domain/reception/repository/ReceptionRepository.java
+++ src/main/java/com/se/sos/domain/reception/repository/ReceptionRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReceptionRepository extends JpaRepository<Reception, UUID> {
     Page<Reception> findByHospital_Id(UUID hospitalId, Pageable pageable);
+
+    Optional<Reception> findReceptionById(UUID receptionId);
 }
