e7e830574668186f284fd96f311c931cf301f247
gojungsu <gjs4565@naver.com>
2024-10-08 19:50:57 +0900

    feat: ReceptionController 로직 추가

--- src/main/java/com/se/sos/domain/reception/controller/ReceptionController.java
+++ src/main/java/com/se/sos/domain/reception/controller/ReceptionController.java
@@ -1,13 +1,18 @@
 package com.se.sos.domain.reception.controller;
 
 import com.se.sos.domain.reception.dto.ReceptionCreateReq;
+import com.se.sos.domain.reception.dto.VisitReq;
+import com.se.sos.domain.reception.entity.ReceptionStatus;
 import com.se.sos.domain.reception.service.ReceptionService;
+import com.se.sos.global.exception.CustomException;
+import com.se.sos.global.response.error.ErrorType;
 import com.se.sos.global.util.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Controller
@@ -29,5 +34,18 @@ public class ReceptionController {
     @GetMapping("/{receptionId}")
     public ResponseEntity<?> getReceptionDetail(@PathVariable(name = "receptionId") String id) {
         return ResponseEntity.ok().body(receptionService.findReceptionById(id));
+    }
+
+    //거절시 응급실 재접수 , 방문신청 수락 , 거절
+    // status -> 상태로 구분 , 거절시 reception 그대로 전달 ,
+    @PutMapping("/{receptionId}")
+    public ResponseEntity<?> handleVisitRequest(@PathVariable(name = "receptionId") String id,
+                                                @RequestBody VisitReq visitReq) {
+        if(visitReq.getReceptionStatus().equals(ReceptionStatus.APPROVED)){
+            return ResponseEntity.ok().body(receptionService.approvedVisitRequest(id));
+        } else if (visitReq.getReceptionStatus().equals(ReceptionStatus.REJECTED)) {
+            return ResponseEntity.ok().body(receptionService.rejectedVisitRequest(id));
+        }
+        throw new CustomException(ErrorType.INVALID_RECEPTION_STATUS);
     }
 }
