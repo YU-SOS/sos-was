d9e59d8df098548e369a8f0f02b7e2ea270185d4
hky035 <rldud035@naver.com>
2024-10-12 02:05:57 +0900

    feat: Reception API 추가 - 게스트 응급실 접수 조회, 접수 요청 수락/거절, 응급실 접수 도착

--- src/main/java/com/se/sos/domain/reception/service/ReceptionService.java
+++ src/main/java/com/se/sos/domain/reception/service/ReceptionService.java
@@ -4,13 +4,15 @@ import com.se.sos.domain.ambulance.entity.Ambulance;
 import com.se.sos.domain.ambulance.repository.AmbulanceRepository;
 import com.se.sos.domain.hospital.entity.Hospital;
 import com.se.sos.domain.hospital.repository.HospitalRepository;
+import com.se.sos.domain.paramedic.entity.Paramedic;
+import com.se.sos.domain.paramedic.repository.ParamedicRepository;
+import com.se.sos.domain.patient.repository.PatientRepository;
 import com.se.sos.domain.reception.dto.ReceptionCreateReq;
+import com.se.sos.domain.reception.dto.ReceptionGuestRes;
 import com.se.sos.domain.reception.dto.ReceptionRes;
 import com.se.sos.domain.reception.entity.Reception;
 import com.se.sos.domain.reception.entity.ReceptionStatus;
-import com.se.sos.domain.reception.repository.PatientRepository;
 import com.se.sos.domain.reception.repository.ReceptionRepository;
-import com.se.sos.domain.reception.response.PatientRes;
 import com.se.sos.global.exception.CustomException;
 import com.se.sos.global.response.error.ErrorType;
 import jakarta.transaction.Transactional;
@@ -26,43 +28,76 @@ public class ReceptionService {
     private final AmbulanceRepository ambulanceRepository;
     private final HospitalRepository hospitalRepository;
     private final PatientRepository patientRepository;
+    private final ParamedicRepository paramedicRepository;
 
-    public void createReception(ReceptionCreateReq receptionCreateReq, UUID ambulanceId) {
+    @Transactional
+    public void createReception(ReceptionCreateReq req, UUID ambulanceId) {
+
+        String hospitalName = req.getHospitalName();
+        UUID paramedicId = req.getParamedicId();
+
         Ambulance ambulance = ambulanceRepository.findById(ambulanceId)
                 .orElseThrow(() -> new CustomException(ErrorType.AMBULANCE_NOT_FOUND));
-        Hospital hospital = hospitalRepository.findByName(receptionCreateReq.getHospitalName())
+        Hospital hospital = hospitalRepository.findByName(hospitalName)
                 .orElseThrow(() -> new CustomException(ErrorType.HOSPITAL_NOT_FOUND));
+        Paramedic paramedic = paramedicRepository.findById(paramedicId)
+                .orElseThrow(() -> new CustomException(ErrorType.PARAMEDIC_NOT_FOUND));
 
-        Reception reception = ReceptionCreateReq.toEntity(receptionCreateReq, ambulance, hospital);
+        Reception reception = ReceptionCreateReq.toEntity(req, ambulance, hospital, paramedic);
         patientRepository.save(reception.getPatient());
         receptionRepository.save(reception);
     }
 
-    public ReceptionRes findReceptionById(String id) {
-        UUID receptionId = UUID.fromString(id);
-        Reception reception = receptionRepository.findReceptionById(receptionId)
+
+    public ReceptionRes findReceptionById(UUID id) {
+        Reception reception = receptionRepository.findReceptionById(id)
                 .orElseThrow(() -> new CustomException(ErrorType.RECEPTION_NOT_FOUND));
 
         return ReceptionRes.from(reception);
     }
+
+
     @Transactional
-    public PatientRes rejectedVisitRequest(String id) {
-        System.out.println("id = " + id);
-        UUID receptionId = UUID.fromString(id);
-        Reception reception = receptionRepository.findReceptionById(receptionId)
+    public void acceptReception(UUID receptionId, boolean isApproved){
+
+        Reception reception = receptionRepository.findById(receptionId)
                 .orElseThrow(() -> new CustomException(ErrorType.RECEPTION_NOT_FOUND));
 
-        receptionRepository.delete(reception);
-        return PatientRes.from(reception.getPatient());
+        if(isApproved)
+            reception.updateReceptionStatus(ReceptionStatus.MOVE);
+        else
+            reception.updateReceptionStatus(ReceptionStatus.REJECTED); // 되면서 병원 아예 reception에서 삭제?
+
+        receptionRepository.save(reception);
     }
 
     @Transactional
-    public ReceptionRes approvedVisitRequest (String id){
-        UUID receptionId = UUID.fromString(id);
-        Reception reception = receptionRepository.findReceptionById(receptionId)
+    public void reRequestReception(UUID receptionId, UUID hospitalId){
+        Hospital hospital = hospitalRepository.findById(hospitalId)
+                .orElseThrow(() -> new CustomException(ErrorType.HOSPITAL_NOT_FOUND));
+
+        Reception reception = receptionRepository.findById(receptionId)
                 .orElseThrow(() -> new CustomException(ErrorType.RECEPTION_NOT_FOUND));
 
-        reception.updateReceptionStatus(ReceptionStatus.APPROVED);
-        return ReceptionRes.from(reception);
+        reception.updateHospital(hospital);
+
+        receptionRepository.save(reception);
+    }
+
+    @Transactional
+    public void closeReception(UUID receptionId){
+        Reception reception = receptionRepository.findById(receptionId)
+                .orElseThrow(() -> new CustomException(ErrorType.RECEPTION_NOT_FOUND));
+
+        reception.closeReception();
+
+        receptionRepository.save(reception);
+    }
+
+    public ReceptionGuestRes findReceptionForGuest(UUID id){
+        Reception reception = receptionRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorType.RECEPTION_NOT_FOUND));
+
+        return ReceptionGuestRes.of(reception.getHospital(), reception.getAmbulance());
     }
 }
