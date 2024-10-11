f81df9ee3644a7befaa1746f7343d2d10538136c
hky035 <rldud035@naver.com>
2024-10-12 02:02:23 +0900

    chore: 불필요 파일 삭제

--- src/main/java/com/se/sos/domain/reception/dto/VisitReq.java
+++
@@ -1,9 +0,0 @@
-package com.se.sos.domain.reception.dto;
-
-import com.se.sos.domain.reception.entity.ReceptionStatus;
-import lombok.Getter;
-
-@Getter
-public class VisitReq {
-    private ReceptionStatus receptionStatus;
-}
