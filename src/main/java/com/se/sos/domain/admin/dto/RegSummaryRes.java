2e871b3940721a76756a8443daea1305810124f8
gojungsu <gjs4565@naver.com>
2024-10-14 15:37:26 +0900

    fix: admin 요청 목록에서 role 제거

--- src/main/java/com/se/sos/domain/admin/dto/RegSummaryRes.java
+++ src/main/java/com/se/sos/domain/admin/dto/RegSummaryRes.java
@@ -2,7 +2,6 @@ package com.se.sos.domain.admin.dto;
 
 import com.se.sos.domain.ambulance.entity.Ambulance;
 import com.se.sos.domain.hospital.entity.Hospital;
-import com.se.sos.domain.user.entity.Role;
 
 import java.util.UUID;
 
@@ -10,15 +9,14 @@ public record RegSummaryRes(
         UUID id,
         String name,
         String address,
-        String telephoneNumber,
-        Role role
+        String telephoneNumber
 ) {
     public static RegSummaryRes fromHospital(Hospital hospital){
-        return new RegSummaryRes(hospital.getId(), hospital.getName(), hospital.getAddress(), hospital.getTelephoneNumber(), hospital.getRole());
+        return new RegSummaryRes(hospital.getId(), hospital.getName(), hospital.getAddress(), hospital.getTelephoneNumber());
     }
 
     public static RegSummaryRes fromAmbulance(Ambulance ambulance){
-        return new RegSummaryRes(ambulance.getId(), ambulance.getName(), ambulance.getAddress(), ambulance.getTelephoneNumber(), ambulance.getRole());
+        return new RegSummaryRes(ambulance.getId(), ambulance.getName(), ambulance.getAddress(), ambulance.getTelephoneNumber());
     }
 
 
