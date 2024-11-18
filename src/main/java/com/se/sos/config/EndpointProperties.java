1025855e63ea0a6bb411b9e829bf336cc5fdefdc
hky035 <rldud035@naver.com>
2024-11-18 14:07:58 +0900

    feat: 일반 사용자 병원 조회 API 추가

--- src/main/java/com/se/sos/config/EndpointProperties.java
+++ src/main/java/com/se/sos/config/EndpointProperties.java
@@ -14,7 +14,6 @@ public class EndpointProperties {
             "/ambulance/**",
             "/reception",
             "/reception/{receptionId}/re",
-            "/hospital",
     };
 
     public static final String[] HOS_EP = {
