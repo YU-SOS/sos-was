17504792e231a6bc420634e69bab006c926a5c85
hky035 <rldud035@naver.com>
2024-11-19 18:32:58 +0900

    fix: SoC 원칙에 따른 메서드 로직 및 리턴값 수정

--- src/main/java/com/se/sos/domain/auth/service/AuthService.java
+++ src/main/java/com/se/sos/domain/auth/service/AuthService.java
@@ -4,10 +4,7 @@ import com.se.sos.domain.admin.entity.Admin;
 import com.se.sos.domain.admin.repository.AdminRepository;
 import com.se.sos.domain.ambulance.entity.Ambulance;
 import com.se.sos.domain.ambulance.repository.AmbulanceRepository;
-import com.se.sos.domain.auth.dto.AdminLoginReq;
-import com.se.sos.domain.auth.dto.AmbulanceSignupReq;
-import com.se.sos.domain.auth.dto.HospitalSignupReq;
-import com.se.sos.domain.auth.dto.UserSignupReq;
+import com.se.sos.domain.auth.dto.*;
 import com.se.sos.domain.category.entity.Category;
 import com.se.sos.domain.category.repository.CategoryRepository;
 import com.se.sos.domain.hospital.entity.Hospital;
@@ -31,7 +28,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.se.sos.global.response.success.SuccessType.LOGIN_SUCCESS;
 import static com.se.sos.global.response.success.SuccessType.LOGOUT_SUCCESS;
@@ -73,7 +72,7 @@ public class AuthService {
         hospitalRepository.save(newHospital);   // 중간테이블까지 업데이트된다.
     }
 
-    public ResponseEntity<?> loginForUser(UserSignupReq req) {
+    public TokenDto loginForUser(UserSignupReq req) {
         String providerUserInfo = req.provider() + " " + req.providerId();
 
         User user = userRepository.findByProviderUserInfo(providerUserInfo);
@@ -91,53 +90,33 @@ public class AuthService {
         String refreshToken = jwtUtil.generateRefreshToken(user.getId().toString(), user.getRole().getValue());
         redisUtil.save(RedisProperties.REFRESH_TOKEN_PREFIX + user.getId(), refreshToken, jwtUtil.getRefreshTokenDuration());
 
-        return ResponseEntity.status(SuccessType.USER_CREATED.getStatus())
-                .header("Set-Cookie", cookieUtil.addRefreshTokenCookie(refreshToken).toString())
-                .header("Authorization", accessToken)
-                .body(SuccessRes.from(SuccessType.LOGIN_SUCCESS));
+        return TokenDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(cookieUtil.addRefreshTokenCookie(refreshToken).toString())
+                .build();
     }
 
-    public ResponseEntity<?> logoutForUser(String accessToken) {
-
+    public boolean logoutForUser(String accessToken) {
         String userId = jwtUtil.parseToken(accessToken).getSubject();
 
-        // AccessToken Redis 블랙리스트 추가 (남은 시간 동안)
         long remainingTime = jwtUtil.extractTokenExpirationDate(accessToken).getTime() - System.currentTimeMillis();
         redisUtil.setBlacklistToken(RedisProperties.ACCESS_TOKEN_PREFIX + userId, remainingTime);
 
-        // RefreshToken Redis에서 삭제
-        boolean deleteRefreshToken = redisUtil.delete(RedisProperties.REFRESH_TOKEN_PREFIX + userId);
-
-        if (deleteRefreshToken) {
-            return ResponseEntity.status(LOGOUT_SUCCESS.getStatus())
-                    .header(HttpHeaders.SET_COOKIE, cookieUtil.deleteRefreshTokenCookie().toString())
-                    .body(SuccessRes.from(LOGOUT_SUCCESS));
-        } else {
-            return ResponseEntity.status(ErrorType.LOGOUT_FAILED.getStatus())
-                    .body(ErrorRes.from(ErrorType.LOGOUT_FAILED));
-        }
+        return redisUtil.delete(RedisProperties.REFRESH_TOKEN_PREFIX + userId); // 삭제 성공 => true
     }
 
-    public ResponseEntity<?> loginForAdmin(AdminLoginReq adminLoginReq) {
+    public String loginForAdmin(AdminLoginReq adminLoginReq) {
 
-        String adminId = adminLoginReq.adminId();
-        System.out.println("adminId = " + adminId);
-        Admin admin = adminRepository.findByAdminId(adminId)
+        Admin admin = adminRepository.findByAdminId(adminLoginReq.adminId())
                 .orElseThrow(() -> new CustomException(ErrorType.UN_AUTHENTICATION));
-        System.out.println("admin = " + admin);
 
-        if(admin.getPassword().equals(adminLoginReq.password())) {
-            String accessToken = jwtUtil.generateAccessToken(admin.getId().toString(), Role.ADMIN.getValue());
-
-            return ResponseEntity.status(LOGIN_SUCCESS.getStatus())
-                    .header(HttpHeaders.AUTHORIZATION, accessToken)
-                    .body(SuccessRes.from(LOGIN_SUCCESS));
-        } else {
+        if(admin.getPassword().equals(adminLoginReq.password()))
+            return jwtUtil.generateAccessToken(admin.getId().toString(), Role.ADMIN.getValue());
+        else
             throw new CustomException(ErrorType.UN_AUTHENTICATION);
-        }
     }
 
-    public ResponseEntity<?> reissueToken(String refreshToken){
+    public TokenDto reissueToken(String refreshToken){
         if(refreshToken == null){
             throw new CustomException(ErrorType.REFRESH_TOKEN_NOT_FOUND);
         }
@@ -153,43 +132,23 @@ public class AuthService {
         if(existedToken != null && refreshToken.equals(existedToken.toString())){
             String newAccessToken = jwtUtil.generateAccessToken(id,role);
             String newRefreshToken = jwtUtil.generateRefreshToken(id,role);
-
             redisUtil.save(key,newRefreshToken, jwtUtil.getRefreshTokenDuration());
 
-            return ResponseEntity
-                    .status(SuccessType.REISSUE_TOKEN_SUCCESS.getStatus())
-                    .header(HttpHeaders.AUTHORIZATION, newAccessToken)
-                    .header(HttpHeaders.SET_COOKIE, cookieUtil.addRefreshTokenCookie(newRefreshToken).toString())
-                    .body(SuccessRes.from(SuccessType.REISSUE_TOKEN_SUCCESS));
-        } else {
-            return ResponseEntity
-                    .status(ErrorType.REISSUE_TOKEN_FAILED.getStatus())
-                    .body(ErrorRes.from(ErrorType.REISSUE_TOKEN_FAILED));
-        }
+            return TokenDto.builder()
+                    .accessToken(newAccessToken)
+                    .refreshToken(cookieUtil.addRefreshTokenCookie(newRefreshToken).toString())
+                    .build();
+
+        } else
+            return null;
 
     }
 
-    public ResponseEntity<?> dupCheck(String id, String role){
-        if(role.equals("AMB")){
-            if(ambulanceRepository.existsByAmbulanceId(id))
-                return ResponseEntity.status(ErrorType.ALREADY_USED_ID.getStatus())
-                        .body(ErrorRes.from(ErrorType.ALREADY_USED_ID));
-            else
-                return ResponseEntity.status(SuccessType.AVAILABLE_ID.getStatus())
-                        .body(SuccessRes.from(SuccessType.AVAILABLE_ID));
+    public boolean dupCheckAmbulanceId(String id) {
+        return ambulanceRepository.existsByAmbulanceId(id);
+    }
 
-
-        } else if (role.equals("HOS")){
-            if(hospitalRepository.existsByHospitalId(id))
-                return ResponseEntity.status(ErrorType.ALREADY_USED_ID.getStatus())
-                        .body(ErrorRes.from(ErrorType.ALREADY_USED_ID));
-            else
-                return ResponseEntity.status(SuccessType.AVAILABLE_ID.getStatus())
-                        .body(SuccessRes.from(SuccessType.AVAILABLE_ID));
-
-        } else {
-            return ResponseEntity.status(ErrorType.BAD_REQUEST.getStatus())
-                    .body(ErrorRes.from(ErrorType.BAD_REQUEST));
-        }
+    public boolean dupCheckHospitalId(String id){
+        return hospitalRepository.existsByHospitalId(id);
     }
 }
