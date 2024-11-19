5b650f2ee18e192af824ff5c053e1ccbcd50bea1
hky035 <rldud035@naver.com>
2024-11-19 15:36:05 +0900

    fix: 불필요 DTO 삭제 및 통합으로 인한 DTO 필드 변경

--- src/main/java/com/se/sos/domain/paramedic/dto/ParamedicRes.java
+++ src/main/java/com/se/sos/domain/paramedic/dto/ParamedicRes.java
@@ -3,13 +3,17 @@ package com.se.sos.domain.paramedic.dto;
 import com.se.sos.domain.paramedic.entity.Paramedic;
 import lombok.Builder;
 
+import java.util.UUID;
+
 @Builder
 public record ParamedicRes(
+        UUID id,
         String name,
         String phoneNumber
 ) {
     public static ParamedicRes fromEntity(Paramedic paramedic) {
         return ParamedicRes.builder()
+                .id(paramedic.getId())
                 .name(paramedic.getName())
                 .phoneNumber(paramedic.getPhoneNumber())
                 .build();
