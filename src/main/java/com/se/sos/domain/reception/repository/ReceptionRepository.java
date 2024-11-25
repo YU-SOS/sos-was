f9bd6bfffd8fd6a59858a38c900895c568664f3c
gojungsu <gjs4565@naver.com>
2024-11-25 18:04:02 +0900

    feat : 병원이 수락한 reception 조회로 인한 로직 추가

--- src/main/java/com/se/sos/domain/reception/repository/ReceptionRepository.java
+++ src/main/java/com/se/sos/domain/reception/repository/ReceptionRepository.java
@@ -1,10 +1,12 @@
 package com.se.sos.domain.reception.repository;
 
 import com.se.sos.domain.reception.entity.Reception;
+import com.se.sos.domain.reception.entity.ReceptionStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,6 +16,7 @@ public interface ReceptionRepository extends JpaRepository<Reception, UUID> {
     Optional<Reception> findReceptionById(UUID receptionId);
     Optional<Reception> findByNumber(String number);
 
+    List<Reception> findByHospital_IdAndReceptionStatusIn(UUID hospitalId, List<ReceptionStatus> receptionStatuses);
     boolean existsByNumber(String number);
 
     long count();
